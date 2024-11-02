39adcdca581d5578abcfec5ec0622f2818df0a7f
yusuf_ma <yusuf_ma@magnit.ru>
2024-11-02 13:06:04 +0300

    fix some bugs and rebuild 1

--- main.py
+++ main.py
@@ -1,42 +1,79 @@
-from parser_products import get_products
-import xlwt
+from parser_products import get_products_from_category
+import openpyxl
+
+
+def remove_all_sheets(path: str) -> None:
+    wb = openpyxl.load_workbook(filename=path,)
+    print(wb.sheetnames)
+    for sheet_name in wb.sheetnames:
+        wb.remove(wb[sheet_name])
+    wb.create_sheet(str(1))
+    wb.save(path)
+
+
+def write_cat(path: str, num: int) -> None:
+    prods = get_products_from_category(num)
+    wb = openpyxl.load_workbook(filename=path,)
+    ws = wb.create_sheet(str(prods["category"]))
+
+    ws.cell(row=1, column=1).value = "Категория"
+    ws.cell(row=1, column=2).value = "Артикул"
+    ws.cell(row=1, column=3).value = "Бренд"
+    ws.cell(row=1, column=4).value = "Наименование товара"
+    ws.cell(row=1, column=5).value = "Цена"
+    ws.cell(row=1, column=6).value = "Описание"
+    ws.cell(row=1, column=7).value = "Ссылки на изображения через запятую"
+
+    row = 2
+    for product in prods["prods"]:
+        ws.cell(row=row, column=1).value = prods["category"]
+        ws.cell(row=row, column=2).value = product["art"]
+        ws.cell(row=row, column=3).value = product["brand"]
+        ws.cell(row=row, column=4).value = product["name_product"]
+        ws.cell(row=row, column=5).value = product["price"]
+        ws.cell(row=row, column=6).value = product["preview"]
+        ws.cell(row=row, column=7).value = product["images"]
+        row += 1
+
+    wb.save(path)
 
 
 def main():
-    prods = get_products()
+    # prods = get_products()
 
-    font0 = xlwt.Font()
-    font0.name = 'Times New Roman'
-    font0.colour_index = 2
-    font0.bold = False
+    path = 'product_catalog.xlsx'
 
-    style0 = xlwt.XFStyle()
-    style0.font = font0
+    remove_all_sheets(path)
 
-    wb = xlwt.Workbook()
-    ws = wb.add_sheet('Каталог товаров')
+    # write_cat(path,123)
+    # write_cat(path,456)
 
-    ws.write(0, 0, "Категория")
-    ws.write(0, 1, "Артикул")
-    ws.write(0, 2, "Бренд")
-    ws.write(0, 3, "Наименование товара")
-    ws.write(0, 4, "Цена")
-    ws.write(0, 5, "Описание")
-    ws.write(0, 6, "Ссылки на изображения через запятую")
+    # get_style()
 
-    row = 1
-    for cat in prods:
-        for product in cat["prods"]:
-            ws.write(row, 0, cat["category"])
-            ws.write(row, 1, product["art"])
-            ws.write(row, 2, product["brand"])
-            ws.write(row, 3, product["name_product"])
-            ws.write(row, 4, product["price"])
-            ws.write(row, 5, product["preview"])
-            ws.write(row, 6, product["images"])
-            row += 1
+    # wb = xlwt.Workbook()
+    # ws = wb.add_sheet('Каталог товаров')
 
-    wb.save('product_catalog.xls')
+    # ws.write(0, 0, "Категория")
+    # ws.write(0, 1, "Артикул")
+    # ws.write(0, 2, "Бренд")
+    # ws.write(0, 3, "Наименование товара")
+    # ws.write(0, 4, "Цена")
+    # ws.write(0, 5, "Описание")
+    # ws.write(0, 6, "Ссылки на изображения через запятую")
+
+    # row = 1
+    # for cat in prods:
+    #     for product in cat["prods"]:
+    #         ws.write(row, 0, cat["category"])
+    #         ws.write(row, 1, product["art"])
+    #         ws.write(row, 2, product["brand"])
+    #         ws.write(row, 3, product["name_product"])
+    #         ws.write(row, 4, product["price"])
+    #         ws.write(row, 5, product["preview"])
+    #         ws.write(row, 6, product["images"])
+    #         row += 1
+
+    # wb.save('product_catalog.xls')
 
 
 main()
--- parser_products.py
+++ parser_products.py
@@ -27,10 +27,11 @@ def get_categories(path: str, products: list) -> None:
         for cat in category.contents:
             if cat.text != "\n":
                 if "name" in cat.attrs["class"]:
-                    category_name = cat.text[1:len(cat.text)-2]
+                    category_name = cat.text[1:len(cat.text)-1]
                 else:
                     for item in cat.contents:
                         if item.text != "\n":
+                            if item.text == "Оборудование Osculati": continue
                             category_text = category_name+" - " + item.text  # category name
                             products.append(
                                 {"category": category_text, "href": item.contents[0].attrs['href']})
@@ -44,7 +45,8 @@ def get_products_hrefs(path: str, product: dict) -> None:
     new_soup = BeautifulSoup(new_req.text, 'lxml')
     items = new_soup.findAll(class_="item-title")
     another_pages_hrefs_raw = new_soup.find(class_="nums")
-    last_page_num = int(another_pages_hrefs_raw.contents[len(
+    last_page_num = 1
+    if another_pages_hrefs_raw is not None: last_page_num = int(another_pages_hrefs_raw.contents[len(
         another_pages_hrefs_raw.contents)-2].text)
     product["prods"] = []
 
@@ -64,15 +66,22 @@ def get_products_info(path: str, product: dict) -> None:
     req = requests.get(path+product["href"])
     soup = BeautifulSoup(req.text, 'lxml')
 
+    product["art"]= ""
     product["art"] = soup.find(
         class_="article iblock").find(class_="value").text
+    
     # есть другие кнопки, влияющие на цену
-    price_raw = soup.find(class_="middle_info wrap_md").find(
-        class_="price").text
-    product["price"] = price_raw[get_index_digit_in_str(
-        price_raw):price_raw.find(".")+1:]
+    product["price"] = ""
+    price_raw_1 = soup.find(class_="middle_info wrap_md").find(
+        class_="price")
+    if price_raw_1 is not None: # может быть пустым
+        price_raw = price_raw_1.text
+        product["price"] = price_raw[get_index_digit_in_str(
+            price_raw):price_raw.find(".")+1:]
 
-    product["preview"] = soup.find(class_="preview_text").text
+    product["preview"] = ""
+    preview_rav = soup.find(class_="preview_text")
+    if preview_rav is not None: product["preview"] = preview_rav.text
 
     images_text = ""
     main_image_src = soup.findAll('img')[1].attrs["src"]
@@ -82,12 +91,13 @@ def get_products_info(path: str, product: dict) -> None:
     # остальные изображения подгружаются позже, пока нет идей как можно их получить
 
     product["brand"] = ""
-    brand_href = soup.find(class_="brand iblock").find(
-        class_="brand_picture").attrs["href"]
-    if brand_href is not None:
-        beand_req = requests.get(path+brand_href)
-        brand_soup = BeautifulSoup(beand_req.text, 'lxml')
-        product["brand"] = brand_soup.find('h1', {"id": "pagetitle"}).text
+    brand_href_block = soup.find(class_="brand iblock")
+    if brand_href_block is not None:
+        brand_href = brand_href_block.find(class_="brand_picture").attrs["href"]
+        if brand_href is not None:
+            beand_req = requests.get(path+brand_href)
+            brand_soup = BeautifulSoup(beand_req.text, 'lxml')
+            product["brand"] = brand_soup.find('h1', {"id": "pagetitle"}).text
 
 
 def get_products() -> list:
@@ -107,3 +117,22 @@ def get_products() -> list:
             get_products_info(path, product)
 
     return products
+
+def get_products_from_category(bum) -> dict:
+    path = "https://yacht-parts.ru"
+    products = []
+    # get_categories(path, products)
+    test_prods_1 = {
+        "category":bum,
+        "prods":[
+            {
+                "art":"sdf art",
+                "brand":"v brand",
+                "name_product":"sdf name_product",
+                "price":" zxcv price",
+                "preview":"v preview",
+                "images":"vs images"
+            }
+        ]
+    }
+    return test_prods_1
